dea421bec16c18bec38a53a0c04d71c74539b289
Chung Nguyen <nvchung.zmg@gmail.com>
2023-08-01 15:30:53 +0700

    add loading

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
-'use client';
+"use client";
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import loading from "./Animation - 1690875954453.gif";
+import "./globals.css";
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push("http://tikmaster.vercel.app")
-  }, [])
-  return (
-    <></>
-  )
+   const router = useRouter();
+   useEffect(() => {
+      setTimeout(() => {
+         router.push("http://tikmaster.vercel.app");
+      }, 2000);
+   }, []);
+   return (
+      <div className="container_app">
+         <Image src={loading} alt="loading" width={200} height={200} />
+         <p>Please wait ...</p>
+      </div>
+   );
 }
